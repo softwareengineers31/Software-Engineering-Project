e8e8e741445b46774194059b12b39f8f4036b89f
ataorm1 <ataorm1@students.towson.edu>
2024-12-01 14:29:17 -0500

    More changes.

--- tuhousing/src/components/SignUp/signup.js
+++ tuhousing/src/components/SignUp/signup.js
@@ -213,7 +213,6 @@ const SignupForm = () => {
                 <button
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 rounded-md shadow-md hover:bg-blue-600"
-                    onClick={() => fetchData()}
                 >
                     Create New Account
                 </button>
